e3619d17ce35e389742c37309d568e9b18a66db9
Cameron-Stewart Smart <cameron.smart@hotmail.co.uk>
2022-03-28 13:34:38 +0100

    Cloned base project
    I cloned the base project into the folder SnakeRuleBased to be adapted to use the rule based AI.

---
+++ SnakeRuleBased/SnakeRuleBased/SnakeBaseProject/Game.h
@@ -0,0 +1,24 @@
+// Cameron-Stewart Smart
+// CMP 304 Unit 1 Assessment
+
+#pragma once
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+#include <string> 
+
+using namespace std;
+
+void initialiseDisplay();
+void draw(int x, int y, string s);
+void setColor(int fcolor, int bcolor);
+void clearScreen(void);
+void drawSnakeFrame();
+void initialiseGrid();
+void drawGrid();
+int moveLoop();
+int moveSnake(int moveDir);
+int newFruit();
+void endScreen(string score, string moves);
+void updateScore(string score, string moves);
+
+#endif
