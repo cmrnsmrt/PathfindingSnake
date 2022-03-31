c9d3e11a37e28844ff77c225a637e58eb79eb5de
Cameron-Stewart Smart <cameron.smart@hotmail.co.uk>
2022-03-31 13:13:16 +0100

    Created finite state machine project
    I copied the rule based system project, removed the rule based system code and renamed it. This will be used to hold the finite state machine version of the game.

---
+++ SnakeFiniteState/SnakeFiniteState/SnakeBaseProject/Source.cpp
@@ -0,0 +1,59 @@
+// Cameron-Stewart Smart
+// CMP 304 Unit 1 Assessment
+// 
+// Includes
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <ctype.h>  
+#include <conio.h> 
+#include <windows.h> 
+#include <time.h> 
+#include <string> 
+#include <iostream> 
+#include <sstream>
+#include "Game.h"
+
+using namespace std;
+
+// Globals
+
+int main() {
+
+	int dir = 1;
+	int result;
+	int moves = 0;
+	int score = 0;
+	
+	initialiseDisplay(); // Initialises display
+	clearScreen(); // Clears any text on screen
+	drawSnakeFrame(); // Draws frame for snake
+	initialiseGrid(); // Sets up the snaketrix
+	drawGrid(); // Draws main game contents
+	newFruit(); // Adds starting fruit onto snaketrix and draws
+
+	while (1) { // Loops until game ends
+		Sleep(100);
+		//dir = moveLoop(); // Gets direction from AI or user
+		
+		dir = finiteStateMachine();
+
+		result = moveSnake(dir); // Moves snake
+
+		moves++; // Increments move counter
+		
+		if ((result == 1) or (dir == 5)) { // If game over code is returned from snake move then game over
+			break; // End game
+		}
+
+		if (result == 3) { // If fruit is hit by the snake increase score and add new fruit
+			newFruit(); // New fruit
+			score++; // Increment score counter
+		}
+		updateScore(to_string(score), to_string(moves)); // Reprints score
+	}
+
+	endScreen(to_string(score), to_string(moves)); // Displays end screen once game loop is broken
+
+	return 0; // End
+}
